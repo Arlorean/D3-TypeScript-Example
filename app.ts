d42f36b149cb609724cd729edfc86c29d169b098
Adam Davidson <adam@adam-davidson.com>
2017-01-18 12:02:32 +0000

    Initial Commit
    The code is taken from the excellent article on D3: http://zeroviscosity.com/d3-js-step-by-step/step-1-a-basic-pie-chart
    This was my attempt to learn D3 and took far longer than it should of to get this working with TypeScript and Visual Studio.
    Checking this in in the hope that others don't have to suffer as I did.

---
+++ app.ts
@@ -0,0 +1,42 @@
+﻿import * as d3 from 'd3';
+
+window.onload = () => {
+    interface Data {
+        label: string;
+        count: number;
+    };
+    var dataset: Data[] = [
+        { label: 'Abulia', count: 10 },
+        { label: 'Betelgeuse', count: 20 },
+        { label: 'Cantaloupe', count: 30 },
+        { label: 'Dijkstra', count: 40 }
+    ];
+
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
+    var svg = d3.select('#chart')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+    
+    var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+    var pie = d3.pie<Data>()
+        .value(d => d.count)
+        .sort(null);
+
+    var path = svg.selectAll('path')
+        .data(pie(dataset))
+        .enter()
+        .append('path')
+        .attr('d', <any>arc)
+        .attr('fill', (d, i) => color(((<any>d.data).label)));
+};
